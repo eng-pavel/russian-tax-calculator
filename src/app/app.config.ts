6080280f97b223e3e1a5b73de3d3e24c44b66527
Pavel Pogorelov <engineer.pavel@yandex.com>
2025-10-23 17:04:04 +0300

    тема primeNG

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,11 +6,20 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
   ],
 };
